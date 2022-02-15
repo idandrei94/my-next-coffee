1875f665143c609d8252101d02d5b2024c3883ae
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-02-15 19:35:46 +0100

    Added the static Oslo pages back

--- pages/index.js
+++ pages/index.js
@@ -64,78 +64,105 @@ export default function Home(props) {
       <Head>
         <title>My Next Coffee | Home</title>
       </Head>
-      <main
-        className={
-          isFindingLocation || coffeeStores.length > 0
-            ? 'flex flex-col items-center justify-between bg-coffee-green bg-coffee-pattern bg-cover bg-center min-h-[90vh] md:min-h-min py-10 md:p-10 lg:px-16 md:pb-20'
-            : 'flex flex-col md:flex-row items-center justify-between bg-coffee-green bg-coffee-pattern bg-cover bg-center min-h-[90vh] md:min-h-min py-10 md:p-10 lg:px-16 md:pb-20'
-        }
-      >
-        <Banner
-          buttonText={
-            isFindingLocation ? (
-              <>
-                <FontAwesomeIcon icon={fasFaCoffee} spin />
-              </>
-            ) : (
-              <>Find My Café</>
-            )
+      <main className="bg-center bg-cover bg-coffee-green bg-coffee-pattern">
+        <div
+          className={
+            isFindingLocation || coffeeStores.length > 0
+              ? 'flex flex-col items-center justify-between min-h-[90vh] md:min-h-min py-10 md:p-10 lg:px-16 md:pb-20'
+              : 'flex flex-col md:flex-row items-center justify-between min-h-[90vh] md:min-h-min py-10 md:p-10 lg:px-16 md:pb-20'
           }
-          handleOnClick={handleOnBannerBtnClick}
-        />
+        >
+          <Banner
+            buttonText={
+              isFindingLocation ? (
+                <>
+                  <FontAwesomeIcon icon={fasFaCoffee} spin />
+                </>
+              ) : (
+                <>Find My Café</>
+              )
+            }
+            handleOnClick={handleOnBannerBtnClick}
+          />
 
-        {isFindingLocation || coffeeStores.length > 0 ? (
-          <></>
-        ) : (
-          <div className="hidden md:flex md:items-center md:justify-center container mx-auto xl:w-3/5 w-full md:p-0 md:w-1/2 md:pl-6">
+          {isFindingLocation || coffeeStores.length > 0 ? (
+            <></>
+          ) : (
+            <div className="container hidden w-full mx-auto md:flex md:items-center md:justify-center xl:w-3/5 md:p-0 md:w-1/2 md:pl-6">
+              {/* Video */}
+              <VideoPlayer />
+            </div>
+          )}
+
+          {locationErrorMsg && (
+            <p className="text-xl text-coffee-100">
+              <FontAwesomeIcon icon={faFaceHandOverMouth} beat />
+              &nbsp; Oops! Something went wrong: <br /> {locationErrorMsg}
+            </p>
+          )}
+
+          {coffeeStoresError && (
+            <p className="text-xl text-coffee-100">
+              <FontAwesomeIcon icon={faFaceHandOverMouth} beat />
+              &nbsp; Oops! Something went wrong: <br /> {coffeeStoresError}
+            </p>
+          )}
+
+          {/* Rendered list */}
+          {coffeeStores.length > 0 && (
+            <div className="container w-2/3 sm:w-full sm:px-4 md:px-14">
+              <h1 className="py-2 text-4xl text-coffee-100">
+                Coffee shops near me
+              </h1>
+              <div className="container grid grid-cols-1 py-6 mb-10 sm:grid-cols-2 lg:grid-cols-3 gap-14 sm:w-full">
+                {coffeeStores.map((coffeeStore) => {
+                  return (
+                    <Card
+                      key={coffeeStore.id}
+                      name={coffeeStore.name}
+                      neighbourhood={coffeeStore.neighbourhood}
+                      distance={coffeeStore.distance}
+                      imgUrl={
+                        coffeeStore.imgUrl ||
+                        'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                      }
+                      href={`/coffee-store/${coffeeStore.id}`}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          )}
+
+          <div className="container flex-none block w-5/6 pb-10 mx-auto md:hidden">
             {/* Video */}
             <VideoPlayer />
           </div>
-        )}
-
-        {locationErrorMsg && (
-          <p className="text-coffee-100 text-xl">
-            <FontAwesomeIcon icon={faFaceHandOverMouth} beat />
-            &nbsp; Oops! Something went wrong: <br /> {locationErrorMsg}
-          </p>
-        )}
-
-        {coffeeStoresError && (
-          <p className="text-coffee-100 text-xl">
-            <FontAwesomeIcon icon={faFaceHandOverMouth} beat />
-            &nbsp; Oops! Something went wrong: <br /> {coffeeStoresError}
-          </p>
-        )}
-
-        {/* Rendered list */}
-        {coffeeStores.length > 0 && (
-          <div className="w-2/3 sm:w-full container sm:px-4 md:px-14">
-            <h1 className="text-coffee-100 text-4xl py-2">
-              Coffee shops near me
-            </h1>
-            <div className="py-6 container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14 mb-10 sm:w-full">
-              {coffeeStores.map((coffeeStore) => {
-                return (
-                  <Card
-                    key={coffeeStore.id}
-                    name={coffeeStore.name}
-                    neighbourhood={coffeeStore.neighbourhood}
-                    distance={coffeeStore.distance}
-                    imgUrl={
-                      coffeeStore.imgUrl ||
-                      'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
-                    }
-                    href={`/coffee-store/${coffeeStore.id}`}
-                  />
-                );
-              })}
+        </div>
+        <div className="container mx-auto">
+          {/* Static coffee shops */}
+          {props.coffeeStores.length > 0 && (
+            <div className="container w-2/3 sm:w-full sm:px-4 md:px-14">
+              <h1 className="py-2 text-4xl text-coffee-100">Oslo City</h1>
+              <div className="container grid grid-cols-1 py-6 mb-10 sm:grid-cols-2 lg:grid-cols-3 gap-14 sm:w-full">
+                {props.coffeeStores.map((coffeeStore) => {
+                  return (
+                    <Card
+                      key={coffeeStore.id}
+                      name={coffeeStore.name}
+                      neighbourhood={coffeeStore.neighbourhood}
+                      distance={coffeeStore.distance}
+                      imgUrl={
+                        coffeeStore.imgUrl ||
+                        'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                      }
+                      href={`/coffee-store/${coffeeStore.id}`}
+                    />
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        )}
-
-        <div className="block md:hidden container mx-auto w-5/6 pb-10 flex-none">
-          {/* Video */}
-          <VideoPlayer />
+          )}
         </div>
       </main>
     </div>
